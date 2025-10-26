a356f6c0e8d24105a92c9bdc7c957575f6e8fbc4
Leonardo Sani <leonardo.sani.pro@gmail.com>
2025-10-26 13:31:49 +0100

    fixed aggregator function duplicated player won columns

--- feature_engineering/Aggregator.py
+++ feature_engineering/Aggregator.py
@@ -8,16 +8,29 @@ def generate_features(battle_data: list[dict], flag_test: bool) -> pd.DataFrame:
     """ Takes the raw battle data, generates all features, and joins them
     into a single DataFrame. """
     
+    if flag_test:
 
-    df_list = [
-        avg_effectiveness_1_1(battle_data, difference=True, include_status_moves=False, test=flag_test),
-        category_impact_score(battle_data, difference=True, test=flag_test),
-        avg_stab_multiplier(battle_data, difference=True, test=flag_test),
-        avg_final_HP_pct(battle_data, difference=True, test=flag_test),
-        avg_boost_diff_per_turn(battle_data, difference=True, test=flag_test),
-        avg_stat_diff_per_turn(battle_data, test=flag_test, stats=['hp', 'atk', 'def', 'spa', 'spd', 'spe']),
-        accuracy_basepower_avg(battle_data, difference=True, test=flag_test)
-    ]
+        df_list = [
+            avg_effectiveness_1_1(battle_data, difference=True, include_status_moves=False, test=flag_test),
+            category_impact_score(battle_data, difference=True, test=flag_test),
+            avg_stab_multiplier(battle_data, difference=True, test=flag_test),
+            avg_final_HP_pct(battle_data, difference=True, test=flag_test),
+            avg_boost_diff_per_turn(battle_data, difference=True, test=flag_test),
+            avg_stat_diff_per_turn(battle_data, test=flag_test, stats=['hp', 'atk', 'def', 'spa', 'spd', 'spe']),
+            accuracy_basepower_avg(battle_data, difference=True, test=flag_test)
+        ]
+
+    else:
+        flag_test = True
+        df_list = [
+            avg_effectiveness_1_1(battle_data, difference=True, include_status_moves=False, test=flag_test),
+            category_impact_score(battle_data, difference=True, test= flag_test),
+            avg_stab_multiplier(battle_data, difference=True, test=flag_test),
+            avg_final_HP_pct(battle_data, difference=True, test=flag_test),
+            avg_boost_diff_per_turn(battle_data, difference=True, test=flag_test),
+            avg_stat_diff_per_turn(battle_data, test=flag_test, stats=['hp', 'atk', 'def', 'spa', 'spd', 'spe']),
+            accuracy_basepower_avg(battle_data, difference=True, test=False)
+        ]
 
 
     if not df_list:
